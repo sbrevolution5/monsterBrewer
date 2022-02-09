dc69fa6fdf3d76323b0b0d8303df998f03e0e7f0
sbrevolution5 <sbrevolution5@aim.com>
2022-02-09 14:37:58 -0500

    further encapsulation

--- src/main/java/sb/monsterBrewer/models/Action.java
+++ src/main/java/sb/monsterBrewer/models/Action.java
@@ -1,6 +1,22 @@
 package sb.monsterBrewer.models;
 
 public class Action {
-    public String name;
-    public String description;
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
