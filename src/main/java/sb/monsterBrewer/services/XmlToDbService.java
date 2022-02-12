421d2a93a3fcd38dfdca9f2feea8712f0876b162
sbrevolution5 <sbrevolution5@aim.com>
2022-02-12 11:41:50 -0500

    continue xml to db methods

--- src/main/java/sb/monsterBrewer/services/XmlToDbService.java
+++ src/main/java/sb/monsterBrewer/services/XmlToDbService.java
@@ -14,12 +14,25 @@ public class XmlToDbService {
         res.setEnvironment(source.getEnvironment());
         res.setSpeed(source.getSpeed());
         res.setHp(source.getHp());
+        parseStats(source, res);
+        parseSkills(source, res);
+        return res;
+
+    }
+
+    private void parseSkills(MonsterXml source, Monster res) {
+        String skillList = source.getSkill();
+        //Check if we find each skill in the list
+        //if not found, set to 0
+
+    }
+
+    private void parseStats(MonsterXml source, Monster res) {
         res.setDexterity(Integer.parseInt(source.getDex()));
         res.setStrength(Integer.parseInt(source.getStr()));
         res.setConstitution(Integer.parseInt(source.getCon()));
         res.setCharisma(Integer.parseInt(source.getCha()));
         res.setWisdom(Integer.parseInt(source.getWis()));
         res.setIntelligence(Integer.parseInt(source.getIntel()));
-        return res;
     }
 }
