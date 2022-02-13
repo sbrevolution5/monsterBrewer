96df755656bb4486597d7bb3d0a8f50142e87208
sbrevolution5 <sbrevolution5@aim.com>
2022-02-13 11:12:00 -0500

    adjust for lowercase

--- src/main/java/sb/monsterBrewer/services/XmlToDbService.java
+++ src/main/java/sb/monsterBrewer/services/XmlToDbService.java
@@ -3,8 +3,10 @@ package sb.monsterBrewer.services;
 import sb.monsterBrewer.dtos.MonsterXml;
 import sb.monsterBrewer.models.Monster;
 
+import java.util.Locale;
+
 public class XmlToDbService {
-    public Monster ReadXmlMonster(MonsterXml source){
+    public Monster parseMonster(MonsterXml source){
         Monster res = new Monster();
         res.setName(source.getName());
         res.setDescription(source.getDescription());
@@ -14,8 +16,15 @@ public class XmlToDbService {
         res.setEnvironment(source.getEnvironment());
         res.setSpeed(source.getSpeed());
         res.setHp(source.getHp());
+        res.setPassive(source.getPassive());
         parseStats(source, res);
+        parseSaves(source,res);
         parseSkills(source, res);
+        parseActions(source, res);
+        parseLegendary(source, res);
+        parseTraits(source, res);
+        parseReactions(source, res);
+
         return res;
 
     }
@@ -23,7 +32,73 @@ public class XmlToDbService {
     private void parseSkills(MonsterXml source, Monster res) {
         String skillList = source.getSkill();
         //Check if we find each skill in the list
-        //if not found, set to 0
+        String[] splitSkills = skillList.split(",");
+        for (int i = 0; i < splitSkills.length; i++) {
+            var skill = splitSkills[i].trim();
+            boolean hasNegativeMod = skill.contains("-");
+            var skillParts = skill.split("[+-]");
+            var skillName = skillParts[0];
+            int skillNum = Integer.parseInt(skillParts[1]);
+            if (hasNegativeMod){
+                skillNum *= -1;
+            }
+            switch (skillName.trim().toLowerCase(Locale.ROOT)){
+                case "athletics" :
+                    res.setAthletics(skillNum);
+                    break;
+                case "acrobatics" :
+                    res.setAcrobatics(skillNum);
+                    break;
+                case "arcana" :
+                    res.setArcana(skillNum);
+                    break;
+                case "animal handling" :
+                    res.setAnimalHandling(skillNum);
+                    break;
+                case "deception" :
+                    res.setDeception(skillNum);
+                    break;
+                case "history" :
+                    res.setHistory(skillNum);
+                    break;
+                case "insight" :
+                    res.setInsight(skillNum);
+                    break;
+                case "intimidation" :
+                    res.setIntimidation(skillNum);
+                    break;
+                case "investigation" :
+                    res.setInvestigation(skillNum);
+                    break;
+                case "medicine" :
+                    res.setMedicine(skillNum);
+                    break;
+                case "nature" :
+                    res.setNature(skillNum);
+                    break;
+                case "perception" :
+                    res.setPerception(skillNum);
+                    break;
+                case "performance" :
+                    res.setPerformance(skillNum);
+                    break;
+                case "persuasion" :
+                    res.setPersuasion(skillNum);
+                    break;
+                case "religion" :
+                    res.setReligion(skillNum);
+                    break;
+                case "sleight of hand" :
+                    res.setSleightOfHand(skillNum);
+                    break;
+                case "stealth" :
+                    res.setStealth(skillNum);
+                    break;
+                case "survival" :
+                    res.setSurvival(skillNum);
+                    break;
+            }
+        }
 
     }
 
