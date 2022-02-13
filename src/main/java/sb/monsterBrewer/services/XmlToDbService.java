485a2fad8705227035fe17e0c474044fecd6ef6b
sbrevolution5 <sbrevolution5@aim.com>
2022-02-13 11:34:54 -0500

    Add languages to parser, and search for damage types

--- src/main/java/sb/monsterBrewer/services/XmlToDbService.java
+++ src/main/java/sb/monsterBrewer/services/XmlToDbService.java
@@ -1,12 +1,14 @@
 package sb.monsterBrewer.services;
 
+import com.fasterxml.jackson.core.sym.CharsToNameCanonicalizer;
 import sb.monsterBrewer.dtos.MonsterXml;
+import sb.monsterBrewer.models.DamageSeverity;
 import sb.monsterBrewer.models.Monster;
 
 import java.util.Locale;
 
 public class XmlToDbService {
-    public Monster parseMonster(MonsterXml source){
+    public Monster parseMonster(MonsterXml source) {
         Monster res = new Monster();
         res.setName(source.getName());
         res.setDescription(source.getDescription());
@@ -17,15 +19,116 @@ public class XmlToDbService {
         res.setSpeed(source.getSpeed());
         res.setHp(source.getHp());
         res.setPassive(source.getPassive());
+        res.setLanguages(source.getLanguages());
         parseStats(source, res);
-        parseSaves(source,res);
+        parseSaves(source, res);
         parseSkills(source, res);
         parseActions(source, res);
         parseLegendary(source, res);
         parseTraits(source, res);
         parseReactions(source, res);
+        parseDamageTypes(source, res);
 
         return res;
+
+    }
+
+    private void parseDamageTypes(MonsterXml source, Monster res) {
+        var vuln = source.getVulnerable();
+        var immu = source.getImmune();
+        var resist = source.getResist();
+        var damageTypes = new String[]{
+                "fire",
+                "cold",
+                "lightning",
+                "thunder",
+                "force",
+                "bludgeoning",
+                "piercing",
+                "slashing",
+                "poison",
+                "psychic",
+                "necrotic",
+                "radiant",
+                "acid",
+                "bludgeoning, piercing, slashing from nonmagical attacks"
+        };
+        //TODO: If we find all the phys types with nonmagical, then we shouldn't look for regular phys types
+        for (int i = 0; i < damageTypes.length; i++) {
+            DamageSeverity resType = DamageSeverity.DEFAULT;
+            if (vuln.contains(damageTypes[i].toLowerCase(Locale.ROOT))) {
+                resType = DamageSeverity.VULNERABLE;
+            } else if (resist.contains(damageTypes[i].toLowerCase(Locale.ROOT))) {
+                resType = DamageSeverity.RESISTANT;
+            } else if (immu.contains(damageTypes[i].toLowerCase(Locale.ROOT))) {
+                resType = DamageSeverity.IMMUNE;
+            }
+            switch (damageTypes[i]) {
+                case "fire":
+                    res.setFire(resType);
+                break;
+                case "cold":
+                    res.setCold(resType);
+                    break;
+                case "lightning":
+                    res.setLightning(resType);
+                    break;
+                case "thunder":
+                    res.setThunder(resType);
+                    break;
+                case "force":
+                    res.setForce(resType);
+                    break;
+                case "bludgeoning":
+                    res.setBludgeoning(resType);
+                    break;
+                case "piercing":
+                    res.setPiercing(resType);
+                    break;
+                case "slashing":
+                    res.setSlashing(resType);
+                    break;
+                case "poison":
+                    res.setPoison(resType);
+                    break;
+                case "psychic":
+                    res.setPsychic(resType);
+                    break;
+                case "necrotic":
+                    res.setNecrotic(resType);
+                    break;
+                case "radiant":
+                    res.setRadiant(resType);
+                    break;
+                case "acid":
+                    res.setAcid(resType);
+                    break;
+                case "bludgeoning, piercing, slashing from nonmagical attacks":
+                    res.setNonMagicSlashing(resType);
+                    res.setNonMagicBludgeoning(resType);
+                    res.setNonMagicPiercing(resType);
+                    break;
+            }
+        }
+    }
+
+    private void parseTraits(MonsterXml source, Monster res) {
+
+    }
+
+    private void parseLegendary(MonsterXml source, Monster res) {
+
+    }
+
+    private void parseReactions(MonsterXml source, Monster res) {
+
+    }
+
+    private void parseActions(MonsterXml source, Monster res) {
+
+    }
+
+    private void parseSaves(MonsterXml source, Monster res) {
 
     }
 
@@ -39,62 +142,62 @@ public class XmlToDbService {
             var skillParts = skill.split("[+-]");
             var skillName = skillParts[0];
             int skillNum = Integer.parseInt(skillParts[1]);
-            if (hasNegativeMod){
+            if (hasNegativeMod) {
                 skillNum *= -1;
             }
-            switch (skillName.trim().toLowerCase(Locale.ROOT)){
-                case "athletics" :
+            switch (skillName.trim().toLowerCase(Locale.ROOT)) {
+                case "athletics":
                     res.setAthletics(skillNum);
                     break;
-                case "acrobatics" :
+                case "acrobatics":
                     res.setAcrobatics(skillNum);
                     break;
-                case "arcana" :
+                case "arcana":
                     res.setArcana(skillNum);
                     break;
-                case "animal handling" :
+                case "animal handling":
                     res.setAnimalHandling(skillNum);
                     break;
-                case "deception" :
+                case "deception":
                     res.setDeception(skillNum);
                     break;
-                case "history" :
+                case "history":
                     res.setHistory(skillNum);
                     break;
-                case "insight" :
+                case "insight":
                     res.setInsight(skillNum);
                     break;
-                case "intimidation" :
+                case "intimidation":
                     res.setIntimidation(skillNum);
                     break;
-                case "investigation" :
+                case "investigation":
                     res.setInvestigation(skillNum);
                     break;
-                case "medicine" :
+                case "medicine":
                     res.setMedicine(skillNum);
                     break;
-                case "nature" :
+                case "nature":
                     res.setNature(skillNum);
                     break;
-                case "perception" :
+                case "perception":
                     res.setPerception(skillNum);
                     break;
-                case "performance" :
+                case "performance":
                     res.setPerformance(skillNum);
                     break;
-                case "persuasion" :
+                case "persuasion":
                     res.setPersuasion(skillNum);
                     break;
-                case "religion" :
+                case "religion":
                     res.setReligion(skillNum);
                     break;
-                case "sleight of hand" :
+                case "sleight of hand":
                     res.setSleightOfHand(skillNum);
                     break;
-                case "stealth" :
+                case "stealth":
                     res.setStealth(skillNum);
                     break;
-                case "survival" :
+                case "survival":
                     res.setSurvival(skillNum);
                     break;
             }
