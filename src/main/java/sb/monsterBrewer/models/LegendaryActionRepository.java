cf4a31f30d76e07600bab843025acf794856f967
sbrevolution5 <sbrevolution5@aim.com>
2022-02-14 13:19:37 -0500

    remove file?

--- src/main/java/sb/monsterBrewer/models/LegendaryActionRepository.java
+++
@@ -1,6 +0,0 @@
-package sb.monsterBrewer.models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LegendaryActionRepository extends JpaRepository<LegendaryAction, Long> {
-}
