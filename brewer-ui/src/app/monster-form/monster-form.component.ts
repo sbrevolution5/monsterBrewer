74381be4274dbfc95546f2fdc5665f766eeb4797
sbrevolution5 <Sbrevolution5@aim.com>
2022-07-21 14:47:15 -0400

    import boilerplate form

---
+++ brewer-ui/src/app/monster-form/monster-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monster-form',
+  templateUrl: './monster-form.component.html',
+  styleUrls: ['./monster-form.component.css']
+})
+export class MonsterFormComponent implements OnInit {
+  originalUserSettings : MonsterData = {
+    name: 'Seth',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'notes go here!'
+  }
+  userSettings : MonsterData={...this.originalUserSettings};
+  postError = false;
+  postErrorMessage = '';
+  constructor(private dataService: DataService) { }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSubmit(form:any){
+    console.log("submitted form");
+  }
+
+}
