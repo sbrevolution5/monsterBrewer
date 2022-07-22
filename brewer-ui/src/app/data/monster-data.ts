4416c729129c5d83da5feba333cc175cfac43bfb
sbrevolution5 <Sbrevolution5@aim.com>
2022-07-22 10:01:18 -0400

    add more properties

--- brewer-ui/src/app/data/monster-data.ts
+++ brewer-ui/src/app/data/monster-data.ts
@@ -6,5 +6,18 @@ export interface MonsterData {
     hitDice: string,
     cr:string,
     alignment: string,
+    strength: number,
+    dexterity: number,
+    constitution: number,
+    wisdom: number,
+    intelligence: number,
+    charisma: number,
+    skills: Skills,
+    damageTypes: DamageTypes,
+    savingThrows: SavingThrows,
+    actions: Action[],
+    legendaryActions: Action[],
+    traits: Trait[],
+    reaction: Action[]
 
 }
