1d602b14dcc155d3ced9b02c821c80019477ad0a
sbrevolution5 <Sbrevolution5@aim.com>
2022-07-22 10:16:41 -0400

    add trait

--- brewer-ui/src/app/monster-form/monster-form.component.ts
+++ brewer-ui/src/app/monster-form/monster-form.component.ts
@@ -1,3 +1,4 @@
+import { Skills } from './../data/skills';
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data/data.service';
 import { MonsterData } from '../data/monster-data';
@@ -8,6 +9,26 @@ import { MonsterData } from '../data/monster-data';
   styleUrls: ['./monster-form.component.css']
 })
 export class MonsterFormComponent implements OnInit {
+  originalSkills: Skills={
+    acrobatics: 0,
+    animalHandling: 0,
+    arcana: 0,
+    athletics: 0,
+    deception: 0,
+    history: 0,
+    insight: 0,
+    intimidation: 0,
+    investigation: 0,
+    medicine: 0,
+    nature: 0,
+    perception: 0,
+    performance: 0,
+    persuasion: 0,
+    religion: 0,
+    sleightOfHand: 0,
+    stealth: 0,
+    survival: 0
+  }
   originalMonsterData : MonsterData = {
     name: 'Seth',
     ac: '10',
@@ -28,7 +49,7 @@ export class MonsterFormComponent implements OnInit {
     intelligenceSave:0,
     legendaryActions:[],
     reactions:[],
-    skills: {},
+    skills: this.originalSkills,
     strength:10,
     strengthSave: 0,
     traits: [],
